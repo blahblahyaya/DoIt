e34af23d9728a84c2526b723e4fc5675571d4c77
Andrew Roberts <andy@note15.com>
2018-03-29 17:26:15 -0700

    add detail view controller, + and back buttons

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Andrew Roberts on 3/29/18.
+//  Copyright © 2018 Andrew Roberts. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
