68852d2e1e4122f747359f4bdfb03336903e4826
Andrew Roberts <andy@note15.com>
2018-03-29 17:51:07 -0700

    stub in create task view controller

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Andrew Roberts on 3/29/18.
+//  Copyright © 2018 Andrew Roberts. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
