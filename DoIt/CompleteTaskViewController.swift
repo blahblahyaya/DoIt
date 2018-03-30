3c5f9e0179d020d8462055832a8b4809cae93ec7
Andrew Roberts <andy@note15.com>
2018-03-29 21:22:28 -0700

    complete w/o core data

---
+++ DoIt/CompleteTaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CompleteTaskViewController.swift
+//  DoIt
+//
+//  Created by Andrew Roberts on 3/29/18.
+//  Copyright © 2018 Andrew Roberts. All rights reserved.
+//
+
+import UIKit
+
+class CompleteTaskViewController: UIViewController {
+     @IBOutlet weak var taskLabel: UILabel!
+    
+    var task = Task()
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if task.important {
+            taskLabel.text = "❗️\(task.name)"
+        } else {
+            taskLabel.text = task.name
+        }
+    }
+
+    @IBAction func completeTapped(_ sender: Any) {
+        previousVC.tasks.remove(at: previousVC.selectedIndex)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+    }
+}
